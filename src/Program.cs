5d9985d1c8babcee465d92c0650f363e43414013
Almuhannad <almuhannad.khalid@gmail.com>
2024-03-18 11:05:37 +0300

    first commit

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Enter a temperature and its unit (C or F):");
+
+
+int temperature = int.Parse(Console.ReadLine());
+Console.WriteLine(temperature);
+
+class TemperatureConverter
+{
+    public string TempConvert(int temperature, string unit) 
+    {
+        if(unit.ToUpper() == "F") {
+            float result = ((temperature - 32) * 5/9);
+            return result + "C";
+        } else {
+            float result = (temperature * 9/5) + 32;
+            return result + "F";
+        }
+    }
+
+}
