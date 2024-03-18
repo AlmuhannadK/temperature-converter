eea17515df784691f33f95e3b9f2e57269c898bc
Almuhannad <almuhannad.khalid@gmail.com>
2024-03-18 13:35:52 +0300

    added user prompt for input (requirement 2)

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,31 @@
-﻿ class Program
+﻿class Program
 {
     public static void Main(string[] args)
     {
-        string TempConvert(int temperature, string unit) 
-        {
-            if(unit.ToUpper() == "F") {
-                double result = ((temperature - 32) * 5/9);
-                    return (result + " C"); 
-            } else {
-                double result = ((temperature * 9/5) + 32);
-                return (result + " F"); 
-            }
-        }
+        Console.WriteLine("Enter a temperature and its unit (C or F):");
 
-        string result = TempConvert(100, "c");
-        Console.WriteLine(result);
+        string? input = Console.ReadLine();
+
+        string[] inputArr = input.Split(" ");
+        double temperature = double.Parse(inputArr[0]);
+        string unit = inputArr[1];
+        string output = TempConvert(temperature, unit);
+
+        Console.WriteLine($"Converted: {input} = {output}");
+
+    }
+
+    static string TempConvert(double temperature, string unit)
+    {
+        if (unit.ToUpper() == "F")
+        {
+            double result = ((temperature - 32) * 5 / 9);
+            return (result + " C");
+        }
+        else
+        {
+            double result = ((temperature * 9 / 5) + 32);
+            return (result + " F");
+        }
     }
 }
