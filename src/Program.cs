c3998d9606ff57a042c41fd94992ccd328d8dac0
Almuhannad <almuhannad.khalid@gmail.com>
2024-03-18 22:39:19 +0300

    added exception handling and continuous interaction (requirement 3)

--- src/Program.cs
+++ src/Program.cs
@@ -4,52 +4,55 @@
     {
         while (true)
         {
-            // - Implement comprehensive error handling for invalid inputs (non-numeric values, unsupported scales).
             Console.WriteLine("Enter a temperature and its unit (C or F) or type \"Quit\" to exit: ");
             string? input = Console.ReadLine();
-            if (input.toLower() == "quit" || input == "")
+            if (input is null || input.ToLower() == "quit")
             {
                 break;
             }
-
             string[] inputArr = input.Split(" ");
-            double temperature = 0;
+
+            float temperature = 0;
             try
             {
-                temperature = double.Parse(inputArr[0]);
+                temperature = float.Parse(inputArr[0]);
             }
-            catch (Exception e)
+
+            catch (FormatException e)
             {
-                // throw new ArgumentException("Please enter the right format for temperature");
-                Console.WriteLine("Invalid input. Please enter a numeric temperature.");
+                Console.Write(e.Message);
+                Console.WriteLine(" Please enter a numeric temperature.");
                 continue;
             }
-            // using if else to check whether the number could be is number or not , if not, throw error and break; 
-            string unit = unit = inputArr[1];
 
-            // Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.
-            string output = TempConvert(temperature, unit);
+            string unit = inputArr[1];
+            string output = "";
+
+            if (unit.ToUpper() == "F" || unit.ToUpper() == "C")
+            {
+                output = TempConvert(temperature, unit);
+            }
+            else
+            {
+                Console.WriteLine("Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.");
+                continue;
+            }
 
             Console.WriteLine($"Converted: {input} = {output}");
-
         }
     }
 
-    static string TempConvert(double temperature, string unit)
+    static string TempConvert(float temperature, string unit)
     {
         if (unit.ToUpper() == "F")
         {
-            double result = ((temperature - 32) * 5.0 / 9.0);
+            float result = ((temperature - 32) * 5 / 9);
             return (result + " C");
-        }
-        if (unit.ToUpper() == "C")
-        {
-            double result = ((temperature * 9.0 / 5.0) + 32);
-            return (result + " F");
         }
         else
         {
-            Console.WriteLine("Please enter a valid scale.");
+            float result = ((temperature * 9 / 5) + 32);
+            return (result + " F");
         }
     }
 }
